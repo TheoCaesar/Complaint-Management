498950bcaaf5b1c0f93c9f856d761e9cb46f59e4
TheoCaesar <theocaesar7@gmail.com>
2024-05-19 14:42:34 +0000

    dashboard template

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  extendNav = false;
   constructor() { }
-
   ngOnInit(): void {
   }
 
+  toggle() {
+    this.extendNav = !this.extendNav;
+    console.log("extending nav?", this.extendNav)
+  }
 }
